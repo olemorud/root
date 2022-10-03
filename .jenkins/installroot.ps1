a2808ffce65bdd0e7939024dfd347bca00d55d34
olemorud <ole.kristian.morud@cern.ch>
2022-10-03 15:38:12 +0200

    add branch param to installroot.ps1 +minor changes

--- .jenkins/installroot.ps1
+++ .jenkins/installroot.ps1
@@ -10,30 +10,32 @@ installroot.ps1  -Generator        <Generator> `
 				 -Workdir          <Path>
 #>
 
-param(
-    [string]$Generator = "Visual Studio 17 2022",       # The Generator used to build ROOT, `cmake --help` lists available generators
-    [string]$TargetArch = "x64",                        # ARM, ARM64, Win32, x64
-    [string]$Config = "Release",                        # Debug, MinSizeRel, Optimized, Release, RelWithDebInfo
-    [string]$CMakeInstallPrefix = "$HOME/ROOT/install", # CMAKE_INSTALL_PREFIX
-    [string]$ToolchainVersion = "x64",                  # Version of host tools to use, e.g. x64 or Win32.
-    [string]$Workdir = "$HOME/ROOT"                     # Where to download, setup and install ROOT
-
-)
-$CMakeParams = @()
-if ($Generator)          { $CMakeParams += "-G`"$Generator`"" }
-if ($TargetArch)         { $CMakeParams += "-A `"$TargetArch`"" }
-if ($ToolchainVersion)   { $CMakeParams += "-Thost=`"$ToolchainVersion`"" }
-if ($CMakeInstallPrefix) { $CMakeParams += "-DCMAKE_INSTALL_PREFIX=`"$CMakeInstallPrefix`"" }
-
-
 Push-Location
+
+param(
+    [string]$Branch = "latest-stable", # The github branch of ROOT to build
+    [string]$Config = "Release",       # Debug, MinSizeRel, Optimized, Release, RelWithDebInfo
+    [string]$Generator = "",       	   # The Generator used to build ROOT, `cmake --help` lists available generators
+    [string]$TargetArch = "x64",       # ARM, ARM64, Win32, x64
+    [string]$ToolchainVersion = "x64", # Version of host tools to use, e.g. x64 or Win32.
+    [string]$Workdir = "$HOME/ROOT"    # Where to download, setup and install ROOT
+)
+$CMakeParams = @(
+	"-DCMAKE_INSTALL_PREFIX=`"$Workdir/install`"",
+    "-A `"$TargetArch`"",
+    "-Thost=`"$ToolchainVersion`""
+)
+if ($Generator) {
+	$CMakeParams += "-G`"$Generator`""
+}
+
 
 if(Test-Path -Path "$Workdir/source") {
     Remove-Item -Recurse -Force "$Workdir/source"
 }
-git clone --branch latest-stable --depth=1 https://github.com/root-project/root.git $Workdir/source
+git clone --branch $Branch --depth=1 https://github.com/root-project/root.git $Workdir/source
 
-# -Force does not overwrite directories but supresses warnings
+# -Force does not overwrite directories but supresses errors if dir exists
 New-Item -ItemType Directory -Force -Path "$Workdir/build"
 New-Item -ItemType Directory -Force -Path "$Workdir/install"
 Set-Location "$Workdir/build"
