8daa8192db50c8b318c63899bf980f8468b6b0e1
olemorud <ole.kristian.morud@cern.ch>
2022-10-03 16:45:38 +0200

    add application credential with secret omitted

---
+++ .jenkins/uploadartifacts/rc.sh
@@ -0,0 +1,8 @@
+
+export OS_AUTH_TYPE=v3applicationcredential
+export OS_AUTH_URL=https://keystone.cern.ch/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME="cern"
+export OS_INTERFACE=public
+export OS_APPLICATION_CREDENTIAL_ID=1df1c205e28945519a798b20e9a50bc5
+# export OS_APPLICATION_CREDENTIAL_SECRET= # omitted
