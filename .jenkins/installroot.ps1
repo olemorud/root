2e0870f3246ff8854bfcd2e7ce3427fd89ef4428
olemorud <ole.kristian.morud@cern.ch>
2022-10-03 14:43:04 +0200

    add (hopefully) working powershell install script

--- .jenkins/installroot.ps1
+++ .jenkins/installroot.ps1
@@ -1,12 +1,45 @@
 # POWERSHELL SCRIPT TO INSTALL ROOT ON WINDOWS
+#
+# maintainer: Ole Morud ole.kristian.morud@cern.ch
+#
+# Usage:
+#     installroot.ps1 -Generator        <Generator> `
+#                     -TargetArch       [ARM|ARM64|Win32|x64] `
+#                     -Config           [Debug, MinSizeRel, Optimized, Release, RelWithDebInfo] `
+#                     -ToolchainVersion [x64|Win32]
+#                      -Workdir            <Path>
 
-Where-Object . # crashes before git clone if run on non-windows OS
+param(
+    [string]$Generator = "Visual Studio 17 2022",       # The Generator used to build ROOT, `cmake --help` lists available generators
+    [string]$TargetArch = "x64",                        # ARM, ARM64, Win32, x64
+    [string]$Config = "Release",                        # Debug, MinSizeRel, Optimized, Release, RelWithDebInfo
+    [string]$CMakeInstallPrefix = "$HOME/ROOT/install", # CMAKE_INSTALL_PREFIX
+    [string]$ToolchainVersion = "x64",                  # Version of host tools to use, e.g. x64 or Win32.
+    [string]$Workdir = "$HOME/ROOT"                     # Where to download, setup and install ROOT
 
-git clone --branch latest-stable --depth=1 https://github.com/root-project/root.git source
-mkdir .\build\
-mkdir .\install\
+)
+$CMakeParams = @()
+if ($Generator)          { $CMakeParams += "-G`"$Generator`"" }
+if ($TargetArch)         { $CMakeParams += "-A `"$TargetArch`"" }
+if ($ToolchainVersion)   { $CMakeParams += "-Thost=`"$ToolchainVersion`"" }
+if ($CMakeInstallPrefix) { $CMakeParams += "-DCMAKE_INSTALL_PREFIX=`"$CMakeInstallPrefix`"" }
 
-cd .\build\
-cmake -G"Visual Studio 16 2019" -A x64 -Thost=x64 -DCMAKE_INSTALL_PREFIX=..\build\ ..\source\
 
-cmake --build . --config $OPTIONS --target install
+Push-Location
+
+if(Test-Path -Path "$Workdir/source") {
+    Remove-Item -Recurse -Force "$Workdir/source"
+}
+git clone --branch latest-stable --depth=1 https://github.com/root-project/root.git $Workdir/source
+
+# -Force does not overwrite directories but supresses warnings
+New-Item -ItemType Directory -Force -Path "$Workdir/build"
+New-Item -ItemType Directory -Force -Path "$Workdir/install"
+cd "$Workdir/build"
+
+echo "cmake $CMakeParams `"$Workdir/source/`""
+cmake @CMakeParams "$Workdir/source/"
+cmake --build "$Workdir/build" --config "$Config" --target install
+
+
+Pop-Location
