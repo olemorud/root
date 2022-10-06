8cdc3d77edc179628dccb1c154f4a3a2922e584d
olemorud <ole.kristian.morud@cern.ch>
2022-10-06 15:03:15 +0200

    add powershell download script

---
+++ .jenkins/s3win/download.ps1
@@ -0,0 +1,15 @@
+
+param(
+    [Parameter(Mandatory=$true)]
+    [String]$Filename
+)
+
+$Url='https://s3.cern.ch/swift/v1/ROOT-build-artifacts'
+$Token= & "$PSScriptRoot/auth.ps1"
+$Headers = @{ 'X-Auth-Token' = "$Token" }
+
+Invoke-WebRequest             `
+    -Uri     "$Url/$Filename" `
+    -Method  'GET'            `
+    -OutFile "$Filename"      `
+    -Headers $Headers
