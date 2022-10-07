0632a68bf1cfd413c2ae077029666e110b418ccb
olemorud <ole.kristian.morud@cern.ch>
2022-10-07 09:26:19 +0200

    fix credentials

--- .jenkins/s3win/rc.ps1
+++ .jenkins/s3win/rc.ps1
@@ -1,10 +1,10 @@
 
-$OS_AUTH_TYPE='v3applicationcredential'
-$OS_AUTH_URL='https://keystone.cern.ch/v3'
-$OS_IDENTITY_API_VERSION='3'
-$OS_REGION_NAME='cern'
-$OS_INTERFACE='public'
+$OS_AUTH_TYPE            = 'v3applicationcredential'
+$OS_AUTH_URL             = 'https://keystone.cern.ch/v3'
+$OS_IDENTITY_API_VERSION = '3'
+$OS_REGION_NAME          = 'cern'
+$OS_INTERFACE            = 'public'
 
-# These must be provided externally
-$OS_APPLICATION_CREDENTIAL_ID='1df1c205e28945519a798b20e9a50bc5'
-#$OS_APPLICATION_CREDENTIAL_SECRET='xxxx'
+# Secret must be provided externally
+$OS_APPLICATION_CREDENTIAL_ID     = '1df1c205e28945519a798b20e9a50bc5'
+$OS_APPLICATION_CREDENTIAL_SECRET = $env:OS_APPLICATION_CREDENTIAL_SECRET
