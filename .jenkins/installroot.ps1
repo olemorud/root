eabf73f51e42cfbdac9bfcab24c356951e280c95
olemorud <ole.kristian.morud@cern.ch>
2022-10-10 15:45:39 +0200

    squash

--- .jenkins/installroot.ps1
+++ .jenkins/installroot.ps1
@@ -149,7 +149,9 @@ log tar czf "$Workdir/$ArchiveName" "$Workdir/source" "$Workdir/build" "$Workdir
 
 try {
     log Set-Location "$Workdir"
-    Write-Host "&" "$PSScriptRoot/s3win/upload.ps1" "$ArchiveName"
+    Write-Host @"
+& "$PSScriptRoot/s3win/upload.ps1" "$ArchiveName"
+"@
     $ScriptLog += @"
 & "$PSScriptRoot/s3win/upload.ps1" "$ArchiveName"
 "@
