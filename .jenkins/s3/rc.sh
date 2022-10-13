257a9132b4457a200d85d11f8dec4fa582d72bd8
olemorud <ole.kristian.morud@cern.ch>
2022-10-13 10:52:59 +0200

    squash

--- .jenkins/s3/rc.sh
+++ .jenkins/s3/rc.sh
@@ -4,5 +4,5 @@ export OS_AUTH_URL=https://keystone.cern.ch/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_REGION_NAME="cern"
 export OS_INTERFACE=public
-export OS_APPLICATION_CREDENTIAL_ID=1df1c205e28945519a798b20e9a50bc5
+#export OS_APPLICATION_CREDENTIAL_ID= # omitted
 # export OS_APPLICATION_CREDENTIAL_SECRET= # omitted
