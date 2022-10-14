12192d440b8d9786977672a9c28fd3b62d35974d
olemorud <ole.kristian.morud@cern.ch>
2022-10-14 10:32:32 +0200

    squash

--- .jenkins/installroot.sh
+++ .jenkins/installroot.sh
@@ -52,7 +52,9 @@ fi
 
 cd /tmp/root/build || exit 1
 
-$doGenerate || cmake -DCMAKE_INSTALL_PREFIX=/tmp/root/install /tmp/root/src/  || exit 1 # $OPTIONS
+if $doGenerate; then
+	cmake -DCMAKE_INSTALL_PREFIX=/tmp/root/install /tmp/root/src/  || exit 1 # $OPTIONS
+fi
 cmake --build /tmp/root/build --target install -- -j"$(getconf _NPROCESSORS_ONLN)" || exit 1
 
 
