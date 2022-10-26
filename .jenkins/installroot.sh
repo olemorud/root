dd2c020d2dd6d7d76c11dee60ab22a8b5376a346
olemorud <ole.kristian.morud@cern.ch>
2022-10-26 13:37:39 +0200

    completely refactor unix install script

--- .jenkins/installroot.sh
+++ .jenkins/installroot.sh
@@ -1,12 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# shellcheck source=s3/utils.sh
+# shellcheck source=s3/auth.sh
 
 stubCMake=false
 
-# Setup environment
 this=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-archiveName=$("$this/s3/getbuildname.sh")
+s3token=$("$this/s3/auth.sh")
 doGenerate=!$INCREMENTAL
+
+optionsum=$(printf '%s' "$OPTIONS" | shasum | cut -d ' ' -f 1)
+archiveNamePrefix="$PLATFORM/$BRANCH/$CONFIG/$optionsum/"
+uploadName="$archiveNamePrefix$(date +%F)"
+
+
+# utils.sh defines downloadArchive(), getArchiveNamePrefix(), searchArchive(), uploadArchive()
+source "$this/s3/utils.sh"
 
 
 # Print debugging info, enable tracing
@@ -16,56 +25,72 @@ date
 pwd
 
 
-
-# Erase files from previous builds and create subdir for build artifacts
-rm -rf /tmp/root/*
-mkdir -p "$(dirname "$archiveName")"
-
-
-
-# If incremental build, download and unpack previous build artifacts from S3
-if [ "$INCREMENTAL" = true ]; then
-    "$this/s3/download.sh" "$archiveName"
-
-    if ! tar -xf "$archiveName" -C /; then
-        INCREMENTAL=false
-    fi
+# Check for previous build artifacts
+downloadName=$(searchArchive "$s3token" "$archiveNamePrefix" | head -n 1)
+if [ -z "$downloadName" ]; then
+    INCREMENTAL=false
 fi
 
-if [ "$INCREMENTAL" = false ]; then
-    mkdir -p /tmp/root/build
-    mkdir -p /tmp/root/install
+exit 0
+
+
+cloneFromGit() {
+    mkdir -p /tmp/workspace/build
+    mkdir -p /tmp/workspace/install
 
     git clone -b "$BRANCH" \
-                --single-branch \
-                --depth 1 \
-                https://github.com/root-project/root.git /tmp/root/src
-else
-    git --git-dir=/tmp/root/src fetch
+                    --single-branch \
+                    --depth 1 \
+                    https://github.com/root-project/root.git \
+                    /tmp/workspace/src
+    
+    return $?
+}
+
+
+downloadAndGitPull() {
+    downloadArchive "$s3token" "$downloadName"
+    tar -xf "$archiveNamePrefix" -C / || return 1
+    # ^^ tar will fail if any previous step fails
+
+    git --git-dir=/tmp/workspace/src fetch
+
+    # shellcheck disable=SC1083
     if [ "$(git rev-parse HEAD)" = "$(git rev-parse @{u})" ]; then
         echo "Files are unchanged since last build, exiting"
         exit 0
     else
-        git --git-dir=/tmp/root/src pull || exit 1
+        git --git-dir=/tmp/workspace/src pull || return 1
         doGenerate=true
     fi
+}
+
+
+# If incremental build, download and unpack previous build artifacts from S3
+rm -rf /tmp/workspace/*
+if $INCREMENTAL; then
+    downloadAndGitPull || cloneFromGit
+else
+    cloneFromGit
 fi
 
+
+# Generate if needed and install
 if ! $stubCMake; then
     if $doGenerate; then
-        cmake -S /tmp/root/src -B /tmp/root/build -DCMAKE_INSTALL_PREFIX=/tmp/root/install || exit 1 # $OPTIONS
+        cmake -S /tmp/workspace/src -B /tmp/workspace/build -DCMAKE_INSTALL_PREFIX=/tmp/workspace/install || exit 1 # $OPTIONS
     fi
-    cmake --build /tmp/root/build --target install -- -j"$(getconf _NPROCESSORS_ONLN)" || exit 1
+    cmake --build /tmp/workspace/build --target install -- -j"$(getconf _NPROCESSORS_ONLN)" || exit 1
 else
-    echo "Stubbing CMake step, writing dummy files to /tmp/root/build and /tmp/root/src"
-    echo "build file" > /tmp/root/build/buildfile.txt
-    echo "install file" > /tmp/root/install/installfile.txt
+    echo "Stubbing CMake step, writing dummy files to /tmp/workspace/build and /tmp/workspace/src"
+    echo "build file" > /tmp/workspace/build/buildfile.txt
+    echo "install file" > /tmp/workspace/install/installfile.txt
 fi
 
 
 # Archive and upload build artifacts to S3
-rm -f "$archiveName"
-tar -Pczf "$archiveName" /tmp/root/build/ /tmp/root/install/ /tmp/root/src/
-"$this/s3/upload.sh" "$archiveName"
+rm -f "$uploadName"
+tar -Pczf "$uploadName" /tmp/workspace/build/ /tmp/workspace/install/ /tmp/workspace/src/
+uploadArchive "$s3token" "$uploadName"
 
 
--- .jenkins/s3/getbuildname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Usage: ./getbuildname.sh <config> <cmake options>
-
-optionsum=$(printf '%s' "$1" | shasum | cut -d ' ' -f 1)
-timestamp=$(date +%F)
-
-
-echo "$PLATFORM/$BRANCH/$CONFIG/$optionsum-$timestamp.tar.gz"
