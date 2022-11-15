eb3b13e073583f4af05fa511abbd69f5f419805e
olemorud <ole.kristian.morud@cern.ch>
2022-11-15 17:56:07 +0100

    shellcheck fixes

--- .jenkins/installroot.sh
+++ .jenkins/installroot.sh
@@ -12,7 +12,7 @@ archiveNamePrefix="$PLATFORM/$BRANCH/$CONFIG/$cmakeOptionsHash/"
 uploadName="$archiveNamePrefix$(date +%F).tar.gz"
 
 mkdir /tmp/workspace/
-cd /tmp/workspace/
+cd /tmp/workspace/       || exit 1
 rm -rf /tmp/workspace/*
 
 # utils.sh defines downloadArchive(), getArchiveNamePrefix(), searchArchive(), 
@@ -35,29 +35,28 @@ cloneFromGit() {
 }
 
 downloadAndGitPull() {
-    cd /tmp/workspace
-        local downloadName=$(searchArchive "$s3token" "$archiveNamePrefix" \
-                             | tail -n 1)
+    cd /tmp/workspace || exit 1
+        local downloadName
+        downloadName=$(searchArchive "$s3token" "$archiveNamePrefix" |tail -n 1)
         downloadArchive "$s3token" "$downloadName"
         tar -xf "$downloadName"  || return 1
         # ^^ tar will fail if any previous step fails
 
         ls -la ./src || return 1
         # if the tar file doesn't contain required files this command fails
-    cd -
+    cd - || exit 1
 
-    cd /tmp/workspace/src
+    cd /tmp/workspace/src || exit 1
         git fetch
 
         # shellcheck disable=SC1083
         if [ "$(git rev-parse HEAD)" = "$(git rev-parse @{u})" ]; then
             echo "Files are unchanged since last build, exiting"
-            cd -
             exit 0
         fi
 
         git merge FETCH_HEAD || return 1
-    cd -
+    cd - || exit 1
 }
 
 
@@ -97,8 +96,8 @@ fi
 
 
 # archive and upload
-cd /tmp/workspace/
-    mkdir -p $(dirname "$uploadName")
+cd /tmp/workspace/ || exit $?
+    mkdir -p "$(dirname "$uploadName")"
     rm -f "$uploadName"
     tar -czf "$uploadName" build install src 
 
@@ -106,4 +105,4 @@ cd /tmp/workspace/
     # this relative path terribleness. There is no portable syntax with 
     # absolute paths
     uploadArchive "$s3token" "$uploadName"
-cd -
+cd - || exit $?
