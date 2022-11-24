aba45d550893318df76bb9ae2760d68b9aa2f6c2
olemorud <ole.kristian.morud@cern.ch>
2022-11-24 18:26:08 +0100

    load cmake config from file, style changes

--- .jenkins/installroot.sh
+++ .jenkins/installroot.sh
@@ -3,21 +3,53 @@
 # shellcheck source=s3/utils.sh
 # shellcheck source=s3/auth.sh
 
-stubCMake=false
+stubCMake=false # Skips generation and build when set to true
 
 this=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# ======Load build options from files ======================
+declare -A config
+
+# Load global options from file
+while IFS='=' read -r key value; do
+	if [ ! -z "$key" ] && [ ! -z "$value" ]; then
+		config[$key]=$value
+	fi
+done < "$this/buildconfig/global.txt"
+
+# Overwrite with platform-specific options from file
+while IFS='=' read -r key value; do
+	if [ ! -z "$key" ] && [ ! -z "$value" ]; then
+		config[$key]=$value
+	fi
+done < "$this/buildconfig/$PLATFORM.txt"
+
+# Use dictionary to populate cmake options
+buildOptions=""
+for key in "${!config[@]}"; do
+    buildOptions+="-D$key=${config[$key]} "
+done
+
+# Sort options to make hashing consistent, generate hash of options
+buildOptions=$(echo "$buildOptions" | tr " " "\n" | sort -i | tr "\n" " ")
+buildOptionsHash=$(printf '%s' "$buildOptions" | shasum | cut -d ' ' -f 1)
+
+
+
+# ======== Set s3 related variables and functions ==========
 s3token=$("$this/s3/auth.sh")
-cmakeOptionsHash=$(printf '%s' "$OPTIONS" | shasum | cut -d ' ' -f 1)
-archiveNamePrefix="$PLATFORM/$BRANCH/$CONFIG/$cmakeOptionsHash/"
+archiveNamePrefix="$PLATFORM/$BRANCH/$CONFIG/$buildOptionsHash/"
 uploadName="$archiveNamePrefix$(date +%F).tar.gz"
-
-mkdir -p /tmp/workspace/
-cd /tmp/workspace/       || exit 1
-rm -rf /tmp/workspace/*
-
 # utils.sh defines downloadArchive(), getArchiveNamePrefix(), searchArchive(), 
 # uploadArchive()
 source "$this/s3/utils.sh"
+
+
+
+# ======== Download+pull or clone, generate and install ====
+mkdir -p /tmp/workspace/
+cd /tmp/workspace/       || exit 1
+rm -rf /tmp/workspace/*
 
 cloneFromGit() {
     INCREMENTAL=false
@@ -35,16 +67,11 @@ cloneFromGit() {
 }
 
 downloadAndGitPull() {
-    cd /tmp/workspace || exit 1
-        local downloadName
-        downloadName=$(searchArchive "$s3token" "$archiveNamePrefix" |tail -n 1)
-        downloadArchive "$s3token" "$downloadName"
-        tar -xf "$downloadName"  || return 1
-        # ^^ tar will fail if any previous step fails
-
-        ls -la ./src || return 1
-        # if the tar file doesn't contain required files this command fails
-    cd - || exit 1
+    local downloadName
+    downloadName=$(searchArchive "$s3token" "$archiveNamePrefix" |tail -n 1)
+    downloadArchive "$s3token" "$downloadName"
+    tar -xf "$downloadName"  || return 1
+    # ^^ tar will fail if any previous step fails
 
     cd /tmp/workspace/src || exit 1
         git fetch
@@ -59,13 +86,11 @@ downloadAndGitPull() {
     cd - || exit 1
 }
 
-
 # debugging
 set -o xtrace
 env | sort -i
 date
 pwd
-
 
 # fetch files
 if $INCREMENTAL; then
@@ -74,13 +99,12 @@ else
     cloneFromGit || exit 0
 fi
 
-
 # generate+build
 if ! $stubCMake; then
     if ! $INCREMENTAL; then
         cmake -S /tmp/workspace/src \
               -B /tmp/workspace/build \
-              -DCMAKE_INSTALL_PREFIX=/tmp/workspace/install $OPTIONS || exit 1
+              -DCMAKE_INSTALL_PREFIX=/tmp/workspace/install $buildOptions || exit 1
     fi
 
     cmake --build /tmp/workspace/build \
@@ -95,7 +119,8 @@ else
 fi
 
 
-# archive and upload
+
+# ======== Upload build artifacts to s3 ====================
 cd /tmp/workspace/ || exit $?
     mkdir -p "$(dirname "$uploadName")"
     rm -f "$uploadName"
