32635591a5c55e67e7613c0dab9d07627c5bd5f7
olemorud <ole.kristian.morud@cern.ch>
2022-12-06 11:03:51 +0100

    remove jenkins stuff

--- .jenkins/s3/rc.sh
+++
@@ -1,8 +0,0 @@
-
-export OS_AUTH_TYPE=v3applicationcredential
-export OS_AUTH_URL=https://keystone.cern.ch/v3
-export OS_IDENTITY_API_VERSION=3
-export OS_REGION_NAME="cern"
-export OS_INTERFACE=public
-#export OS_APPLICATION_CREDENTIAL_ID= # omitted
-# export OS_APPLICATION_CREDENTIAL_SECRET= # omitted
